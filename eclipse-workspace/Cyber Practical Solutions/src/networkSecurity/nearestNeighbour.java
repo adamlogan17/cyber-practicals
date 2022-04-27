54c9213ca6f02d14cf48a0e07812e7025fa17c62
Adam Logan <alogan20@qub.ac.uk>
2022-04-27 16:15:43 +0100

    added malware protection code

--- eclipse-workspace/Cyber Practical Solutions/src/networkSecurity/nearestNeighbour.java
+++ eclipse-workspace/Cyber Practical Solutions/src/networkSecurity/nearestNeighbour.java
@@ -1,22 +1,26 @@
 package networkSecurity;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class nearestNeighbour {
 
 	public static void main(String[] args) {
 		int[] nums = {132, 135, 120, 139, 19, 117, 112, 285, 135, 103}; 
-		int[][] ans = getAllNearestNeighbour(nums, 3);
+		int[][] ans = getAllNearestNeighbour(nums, 2);
 		prnt2dArry(ans);
+		
+		int[] ans2 = getNearestNeighbour(nums, 3, 1);
+		System.out.println(prntArry(ans2));
 		
 		ArrayList<Integer[]> ans3 = getAnomalyPoints(nums, 3, 0.3);
 		for(Integer[] x: ans3) {
 			System.out.print("[" + prntArry(x) + "], ");
 		}
+		System.out.println();
 		
-		int[] ans2 = getNearestNeighbour(nums, 3, 1);
-		System.out.println(prntArry(ans2));
+		int[] nums2 = {190, 127, 48, 108, 122, 190, 75, 145, 54, 69}; 
+		int[][] ans4 = getAllNearestNeighbour(nums2, 3);
+		prnt2dArry(ans4);
 	}
 	
 	/**
@@ -43,8 +47,7 @@ public class nearestNeighbour {
 	 * 
 	 * @param nums
 	 * @param k
-	 * @return - the nearest neighbour size and difference, the index number
-	 * of the array corresponds with the index of the original array
+	 * @return - [[id, the nearest neighbour size, difference]]
 	 */
 	private static int[][] getAllNearestNeighbour(int[] nums, int k) {
 		int[][] result = new int[nums.length][3];
@@ -59,16 +62,22 @@ public class nearestNeighbour {
 	 * @param nums
 	 * @param k
 	 * @param pos
-	 * @return [size of kth neighbour, difference]
+	 * @return [id of orig, size of kth neighbour, difference]
 	 */
 	private static int[] getNearestNeighbour(int[] nums, int k, int pos) {
 		int[][] allDiffs = new int[nums.length][3];
 		
 		allDiffs = getDifferences(nums, pos);
 		bubbleSort(allDiffs);
-		return allDiffs[k-1];
+		return allDiffs[k];
 	}
 	
+	/**
+	 * 
+	 * @param nums
+	 * @param pos
+	 * @return [[id of orig, size of the other packet, diff]]
+	 */
 	private static int[][] getDifferences(int[] nums, int pos) {
 		int[][] result = new int[nums.length][3];
 		for(int i=0; i<nums.length; i++) {
